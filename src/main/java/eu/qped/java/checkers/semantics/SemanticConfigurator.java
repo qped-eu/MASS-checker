12761f428a2ea83929033edcbf8bfde0c56d61fb
Ajio <Ajio@students.uni-marburg.de>
2022-04-17 17:04:09 +0200

    Added Annotations

--- src/main/java/eu/qped/java/checkers/semantics/SemanticConfigurator.java
+++ src/main/java/eu/qped/java/checkers/semantics/SemanticConfigurator.java
@@ -1,8 +1,15 @@
 package eu.qped.java.checkers.semantics;
 
 import eu.qped.java.checkers.mass.QFSemSettings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SemanticConfigurator {
 
     private String methodName;
@@ -14,7 +21,7 @@ public class SemanticConfigurator {
     private String doWhileLoop;
     private String returnType;
 
-    private final QFSemSettings qfSemSettings;
+    private QFSemSettings qfSemSettings;
 
 
     private SemanticConfigurator(QFSemSettings qfSemSettings) {
@@ -65,70 +72,6 @@ public class SemanticConfigurator {
             setReturnType(qfSemSettings.getReturnType());
         }
 
-    }
-
-    public String getMethodName() {
-        return methodName;
-    }
-
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
-    }
-
-    public String getRecursionAllowed() {
-        return recursionAllowed;
-    }
-
-    public void setRecursionAllowed(String recursionAllowed) {
-        this.recursionAllowed = recursionAllowed;
-    }
-
-    public String getWhileLoop() {
-        return whileLoop;
-    }
-
-    public void setWhileLoop(String whileLoop) {
-        this.whileLoop = whileLoop;
-    }
-
-    public String getForLoop() {
-        return forLoop;
-    }
-
-    public void setForLoop(String forLoop) {
-        this.forLoop = forLoop;
-    }
-
-    public String getForEachLoop() {
-        return forEachLoop;
-    }
-
-    public void setForEachLoop(String forEachLoop) {
-        this.forEachLoop = forEachLoop;
-    }
-
-    public String getIfElseStmt() {
-        return ifElseStmt;
-    }
-
-    public void setIfElseStmt(String ifElseStmt) {
-        this.ifElseStmt = ifElseStmt;
-    }
-
-    public String getDoWhileLoop() {
-        return doWhileLoop;
-    }
-
-    public void setDoWhileLoop(String doWhileLoop) {
-        this.doWhileLoop = doWhileLoop;
-    }
-
-    public String getReturnType() {
-        return returnType;
-    }
-
-    public void setReturnType(String returnType) {
-        this.returnType = returnType;
     }
 
 }
