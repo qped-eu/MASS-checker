b06d8342d4f086ec67e7a43b700c3035035847e8
Omar Aji <omar3ajii@gmail.com>
2022-07-13 15:37:12 +0200

    fix sem

--- src/main/java/eu/qped/framework/CheckerRunner.java
+++ src/main/java/eu/qped/framework/CheckerRunner.java
@@ -101,11 +101,11 @@ public class CheckerRunner {
 					}
 				}
 				System.out.println(">>>>>>>>>>>>>" + fileInfo.getMimetype());
-				if (fileInfo.getMimetype().equals("application/zip")) {
+				if (fileInfo.getMimetype().equals("application/x-zip-compressed")) {
 					try {
 						File unzipTarget = Files.createTempDirectory("exam-results").toFile();
 						tempFiles.add(unzipTarget);
-
+						System.out.println(">>>>>>>>>>>>>" + fileInfo.getMimetype());
 						ZipFile zipFile = new ZipFile(submittedFile);
 						zipFile.extractAll(unzipTarget.toString());
 
