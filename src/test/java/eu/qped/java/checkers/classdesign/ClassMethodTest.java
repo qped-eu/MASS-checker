def3bb88f07981e991424dd87323b9fa189ab2de
Abdulsalam Alfandi <jack.sb2020@mail.ru>
2023-02-18 09:30:07 +0100

    ClassMethodenTest bearbeiten

--- src/test/java/eu/qped/java/checkers/classdesign/ClassMethodTest.java
+++ src/test/java/eu/qped/java/checkers/classdesign/ClassMethodTest.java
@@ -82,7 +82,7 @@ public class ClassMethodTest {
             e.printStackTrace();
         }
 
-        ClassFeedback fb = TestUtils.getFeedback("class TestClass", "a", ClassFeedbackType.WRONG_ELEMENT_TYPE);
+        ClassFeedback fb = TestUtils.getFeedback("class TestClass", "a()", ClassFeedbackType.WRONG_ELEMENT_TYPE);
         ClassFeedback[] expectedFeedback = new ClassFeedback[] {fb};
         assertArrayEquals(expectedFeedback, classChecker.getClassFeedbacks().toArray(new ClassFeedback[0]));
     }
@@ -105,7 +105,7 @@ public class ClassMethodTest {
             e.printStackTrace();
         }
 
-        ClassFeedback fb = TestUtils.getFeedback("class TestClass", "b", ClassFeedbackType.WRONG_ELEMENT_NAME);
+        ClassFeedback fb = TestUtils.getFeedback("class TestClass", "b()", ClassFeedbackType.WRONG_ELEMENT_NAME);
         ClassFeedback[] expectedFeedback = new ClassFeedback[] {fb};
         assertArrayEquals(expectedFeedback, classChecker.getClassFeedbacks().toArray(new ClassFeedback[0]));
     }
@@ -127,7 +127,7 @@ public class ClassMethodTest {
             e.printStackTrace();
         }
 
-        ClassFeedback fb = TestUtils.getFeedback("class TestClass", "b", ClassFeedbackType.WRONG_ELEMENT_TYPE);
+        ClassFeedback fb = TestUtils.getFeedback("class TestClass", "b()", ClassFeedbackType.WRONG_ELEMENT_TYPE);
         ClassFeedback[] expectedFeedback = new ClassFeedback[] {fb};
         assertArrayEquals(expectedFeedback, classChecker.getClassFeedbacks().toArray(new ClassFeedback[0]));
     }
