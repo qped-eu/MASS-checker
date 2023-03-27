e9b950575ba9fa0a58e3cd230abd20afa1208749
Tobias Altenbrand <altenbrt@students.uni-marburg.de>
2023-03-27 14:00:42 +0200

    Test6

--- src/main/java/eu/qped/racket/RacketChecker.java
+++ src/main/java/eu/qped/racket/RacketChecker.java
@@ -8,10 +8,23 @@ public class RacketChecker implements Checker {
     @Override
     public void check(QfObject qfObject) throws Exception {
         String answer = qfObject.getAnswer();
-        
-        String[] feedback = new String[1];
+
+        String message = "EMPTY";
+
+        try {
+            DrRacketInterpreter inter = new DrRacketInterpreter(answer);
+            message = inter.evaluateExpressions();
+
+        } catch (Exception e) {
+            //System.out.println(e);
+            message = e.getMessage();
+        }
+
+
+        String[] feedback = new String[2];
 
         feedback[0] = "# Hallo Welt";
+        feedback[1] = message;
 
         qfObject.setFeedback(feedback);
     }
