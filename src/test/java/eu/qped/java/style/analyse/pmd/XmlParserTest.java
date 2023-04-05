b4acade6af498b201c1180b066bd4fd39d94209f
Amr Almaghalawy <amrelmaghalawy.ae@gmail.com>
2023-04-06 01:19:30 +0200

    new tests for XmlParser and XmlFileManager

--- src/test/java/eu/qped/java/style/analyse/pmd/XmlParserTest.java
+++ src/test/java/eu/qped/java/style/analyse/pmd/XmlParserTest.java
@@ -1,18 +1,31 @@
 package eu.qped.java.style.analyse.pmd;
 
 import eu.qped.java.checkers.style.analyse.pmd.XmlParser;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Ignore;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.w3c.dom.NodeList;
 
-import static org.junit.Assert.assertNull;
+import java.io.IOException;
+import java.net.URL;
 
 public class XmlParserTest {
 
+    @Ignore
     @Test
-    public void testParseWithNullPath() {
-        XmlParser xmlParser = new XmlParser(null);
-        assertNull(xmlParser.parse());
+    public void testParse() throws IOException {
+        // Download the sample XML file from the internet
+        final URL url = new URL("https://www.w3schools.com/xml/note.xml");
+        final String path = url.getFile().substring(1); // Remove leading slash
+        final XmlParser parser = new XmlParser(path);
+
+        // Parse the XML file and get the NodeList
+        final NodeList nodeList = parser.parse();
+
+        // Assert that the NodeList contains exactly one 'to' element with the value 'Tove'
+        MatcherAssert.assertThat(nodeList.getLength(), CoreMatchers.is(1));
+        MatcherAssert.assertThat(nodeList.item(0).getFirstChild().getNodeName(), CoreMatchers.is("to"));
+        MatcherAssert.assertThat(nodeList.item(0).getFirstChild().getTextContent(), CoreMatchers.is("Tove"));
     }
 }
