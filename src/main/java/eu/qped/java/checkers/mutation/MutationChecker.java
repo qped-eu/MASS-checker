379bf28b87b5a9ef4984e7d51f90f6d04edeb142
sallandcall <berkanaktas2001@gmail.com>
2023-11-06 22:36:39 +0100

    Update MutationChecker.java

--- src/main/java/eu/qped/java/checkers/mutation/MutationChecker.java
+++ src/main/java/eu/qped/java/checkers/mutation/MutationChecker.java
@@ -1,8 +1,7 @@
 package eu.qped.java.checkers.mutation;
 
-import eu.qped.framework.QpedQfFilesUtility;
+import eu.qped.java.checkers.Facade;
 import eu.qped.java.checkers.coverage.MemoryLoader;
-import eu.qped.java.checkers.coverage.framework.coverage.CoverageFacade;
 import eu.qped.java.checkers.coverage.framework.test.JUnit5;
 import eu.qped.java.checkers.coverage.framework.test.TestFramework;
 import eu.qped.java.checkers.mass.QfMutationSettings;
@@ -11,7 +10,6 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -28,10 +26,10 @@ public class MutationChecker {
 
     public List<String> check() {
         // Determine the application classes and test classes in the solution
-        List<CoverageFacade> testClasses = new ArrayList<>();
-        List<CoverageFacade> classes = new ArrayList<>();
+        List<Facade> testClasses = new ArrayList<>();
+        List<Facade> classes = new ArrayList<>();
         try {
-            separateTestAndApplicationClasses(testClasses, classes);
+            Facade.separateTestAndApplicationClasses(solutionRoot,testClasses, classes);
         } catch (IOException e) {
             throw new RuntimeException("Could not create lists of test and application classes.", e);
         }
@@ -44,7 +42,7 @@ public class MutationChecker {
             return Collections.emptyList();
         }
 
-        List<String> testClassNames = testClasses.stream().map(CoverageFacade::className).collect(Collectors.toList());
+        List<String> testClassNames = testClasses.stream().map(Facade::className).collect(Collectors.toList());
         Class<?> mutationInfrastructureClass;
 
         try {
@@ -85,24 +83,23 @@ public class MutationChecker {
 
                 // Create an instance of the loaded class (assuming it has a default constructor)
                 mutationInfrastructureClass = loadMutationInfrastructureClass(memoryLoader);
-                Object mutationInfrastructureInstance;
+
+                // Create an instance of MutationInfrastructureClass using the no-argument constructor
+                Object mutationInfrastructureInstance = mutationInfrastructureClass.getDeclaredConstructor().newInstance();
 
                 // Set the firstTest field to false
                 Field field = mutationInfrastructureClass.getDeclaredField("firstTest");
                 field.setAccessible(true);
-                field.set(null, false);
-
-                // Create an instance of MutationInfrastructureClass using the no-argument constructor
-                mutationInfrastructureInstance = mutationInfrastructureClass.getDeclaredConstructor().newInstance();
+                field.set(mutationInfrastructureInstance, false);
 
                 // Set the correct variants
                 mutationInfrastructureClass.getDeclaredMethod("setCorrectVariants", List.class).invoke(mutationInfrastructureInstance, tmpList);
 
                 // Run test
-                List<String> testResultstmp = test.testing(testClassNames, memoryLoader);
+                testResults = test.testing(testClassNames, memoryLoader);
 
                 // if mutation is not detected, report the defect
-                if (testResultstmp.isEmpty()) {
+                if (testResults.isEmpty()) {
                     messages.add("### Test Mutation Feedback");
                     messages.add("- " + reportDefectiveMutant(mutationInfrastructureClass, i));
                     System.out.println(messages);
@@ -116,53 +113,16 @@ public class MutationChecker {
         return messages;
     }
 
-    public void separateTestAndApplicationClasses(List<CoverageFacade> testClasses, List<CoverageFacade> classes)
-            throws IOException {
-        List<File> allClassFiles = QpedQfFilesUtility.filesWithExtension(solutionRoot, "class");
-        String solutionDirectoryPath = solutionRoot.getCanonicalPath() + File.separator;
-
-        // The file separator will be used as regular expression by replaceAll.
-        // Therefore, we must escape the separator on Windows systems.
-        String fileSeparator = File.separator;
-        if (fileSeparator.equals("\\")) {
-            fileSeparator = "\\\\";
-        }
-        for (File file : allClassFiles) {
-            String filename = file.getCanonicalPath();
-
-            String classname = filename.
-                    substring(solutionDirectoryPath.length(), filename.length() - ".class".length()).
-                    replaceAll(fileSeparator, ".");
-
-            String[] classnameSegments = classname.split("\\.");
-            String simpleClassname = classnameSegments[classnameSegments.length - 1];
-
-            CoverageFacade coverageFacade = new CoverageFacade(
-                    Files.readAllBytes(file.toPath()),
-                    classname);
-
-            if (simpleClassname.startsWith("Test")
-                    || simpleClassname.startsWith("test")
-                    || simpleClassname.endsWith("Test")
-                    || simpleClassname.endsWith("test")) {
-                // the class is a test
-                testClasses.add(coverageFacade);
-            } else {
-                // the class is an application class (i.e., no test)
-                classes.add(coverageFacade);
-            }
-        }
-    }
-
-    private void loadClassesIntoMemory(List<CoverageFacade> testClasses, List<CoverageFacade> classes, MemoryLoader memoryLoader) {
-        for (CoverageFacade testClass : testClasses) {
+    private void loadClassesIntoMemory(List<Facade> testClasses, List<Facade> classes, MemoryLoader memoryLoader) {
+        for (Facade testClass : testClasses) {
             memoryLoader.upload(testClass.className(), testClass.byteCode());
         }
-        for (CoverageFacade clazz : classes) {
+        for (Facade clazz : classes) {
             memoryLoader.upload(clazz.className(), clazz.byteCode());
         }
     }
 
+    @SuppressWarnings("unchecked")
     private int getMutationPossibilities(Class<?> mutationInfrastructureClass) {
         try {
             Field fieldMessage = mutationInfrastructureClass.getDeclaredField("mutationMessageList");
@@ -175,6 +135,7 @@ public class MutationChecker {
         }
     }
 
+    @SuppressWarnings("unchecked")
     private String reportDefectiveMutant(Class<?> mutationInfrastructureClass, int i) throws NoSuchFieldException, IllegalAccessException {
         Field fieldMessage = mutationInfrastructureClass.getDeclaredField("mutationMessageList");
         fieldMessage.setAccessible(true);
